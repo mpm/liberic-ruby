0e7ba0cab25233c47255d0f095f65398f8d631d0
Malte Münchert <malte.muenchert@gmx.net>
2015-11-27 01:56:55 +0100

    Add simple PDF generation flag to Process#execute

--- lib/liberic/process.rb
+++ lib/liberic/process.rb
@@ -11,11 +11,23 @@ module Liberic
       end
     end
 
-    def execute
+    # Executes the actual call to +EricBearbeiteVorgang()+
+    #
+    # The optional +action+ parameter can be set to +print+ to generate a PDF from this case.
+    # Set +Liberic.config.data_path+ to the location where the PDF should be stored.
+    #
+    # Example:
+    #
+    #   filing = Liberic::Process.new(xml_tax_filing, 'ESt_2011')
+    #   filing.execute(action: :print)
+    #
+    # Will generate a PDF in the current directory if not specified otherwise.
+    # *WARNING* method signature and parameters are WIP and subject to change
+    def execute(action: :validate)
       #druck_params = SDK::Types::DruckParameter.new
       Helpers::Invocation.with_result_buffer(false) do |handle|
         SDK::API.bearbeite_vorgang(@xml, @type,
-          :validiere,
+          action == :print ? :drucke : :validiere,
           nil, #druck_params,
           nil,
           nil,
