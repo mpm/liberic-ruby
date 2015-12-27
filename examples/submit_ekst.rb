8dd6eca3c12d7cd911bed209483be694ce9bce13
Malte Münchert <malte.muenchert@gmx.net>
2015-12-27 11:37:47 -0800

    Add first code examples in separate directory

---
+++ examples/submit_ekst.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require "bundler/setup"
+require "liberic"
+
+tax_filing = File.read(File.expand_path('steuersatz.xml', __FILE__))
+
+submission = Liberic::Process.new(tax_filing, 'ESt_2011')
+
+submission.check
+
+result = submission.execute(action: :print_and_execute)
+puts result
